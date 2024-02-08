9a07d5bb7d31522a5ee3cde16dda0fbbeb7e3136
Hon_Dev <mohanadramadan.m1@gmail.com>
2024-02-08 10:33:51 +0200

    Modified the NetworkManager call with Result type.

--- GitHub Followers/FollowersListVC.swift
+++ GitHub Followers/FollowersListVC.swift
@@ -14,12 +14,13 @@ class FollowersListVC: UIViewController {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
         
-        NetworkManager.shared.getFollowers(for: username, page: 1) { followers, error in
-            guard let followers = followers else {
-                self.presentGFAlertOnMainThread(alertTitle: "Shiit", messageText: "fuck this", buttonTitle: "Get the fuck out!")
-                return
+        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+            switch result {
+            case .success(let followers):
+                print(followers)
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(alertTitle: "Opps!", messageText: error.rawValue, buttonTitle: "OK")
             }
-            
         }
     }
     
