6b3eae9dd74bd11625c5248dd1f8cfa0c451844a
Hon_Dev <mohanadramadan.m1@gmail.com>
2024-02-08 10:19:16 +0200

    NetworkManager created with basic fetch method.

--- GitHub Followers/FollowersListVC.swift
+++ GitHub Followers/FollowersListVC.swift
@@ -12,8 +12,20 @@ class FollowersListVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        navigationController?.navigationBar.isHidden = false
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        NetworkManager.shared.getFollowers(for: username, page: 1) { followers, error in
+            guard let followers = followers else {
+                self.presentGFAlertOnMainThread(alertTitle: "Shiit", messageText: "fuck this", buttonTitle: "Get the fuck out!")
+                return
+            }
+            
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)
     }
     
     var username: String!
