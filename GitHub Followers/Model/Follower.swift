2cbc1e564707dec3adefaaf79775ee4890f83b1e
Hon_Dev <mohanadramadan.m1@gmail.com>
2024-02-08 09:14:01 +0200

    Made some refacor to the project and build github models.

---
+++ GitHub Followers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GitHub Followers
+//
+//  Created by Mohanad Ramdan on 08/02/2024.
+//
+
+import Foundation
+
+struct Follower: Codable{
+    var login: String
+    var avatarUrl: String
+}
