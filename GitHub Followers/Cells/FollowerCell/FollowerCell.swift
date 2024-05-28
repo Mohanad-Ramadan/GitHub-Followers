8380310b6721b320029295e5e4ad9cb92cb26e54
Hon_Dev <mohanadramadan.m1@gmail.com>
2024-05-28 21:37:27 +0300

    fix: Incorrect layout for the action button at SearchVC when the keyboard popout

--- GitHub Followers/Cells/FollowerCell/FollowerCell.swift
+++ GitHub Followers/Cells/FollowerCell/FollowerCell.swift
@@ -44,7 +44,5 @@ class FollowerCell: UICollectionViewCell {
     let usernameLabel = GFTitleLabel(textAlignment: .center, fontSize: 16)
     
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+    required init?(coder: NSCoder) {fatalError()}
 }
--- GitHub Followers/SearchVC.swift
+++ GitHub Followers/SearchVC.swift
@@ -55,17 +55,18 @@ class SearchVC: UIViewController {
         let topConstraintConstant: CGFloat = DeviceTypes.isiPhoneSE || DeviceTypes.isiPhone8Zoomed ? 20 : 80
         
         NSLayoutConstraint.activate([
-            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: topConstraintConstant),
+            logoImageView.topAnchor.constraint(lessThanOrEqualTo: view.safeAreaLayoutGuide.topAnchor, constant: topConstraintConstant),
             logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             logoImageView.heightAnchor.constraint(equalToConstant: 200),
             logoImageView.widthAnchor.constraint(equalToConstant: 200),
             
-            usernameTextField.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 48),
+            usernameTextField.topAnchor.constraint(lessThanOrEqualTo: logoImageView.bottomAnchor, constant: 48),
             usernameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
             usernameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
             usernameTextField.heightAnchor.constraint(equalToConstant: 50),
             
-            callToActionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            callToActionButton.topAnchor.constraint(greaterThanOrEqualTo: usernameTextField.bottomAnchor, constant: 10),
+            callToActionButton.bottomAnchor.constraint(equalTo: view.keyboardLayoutGuide.topAnchor, constant: -50),
             callToActionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
             callToActionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
             callToActionButton.heightAnchor.constraint(equalToConstant: 50)
